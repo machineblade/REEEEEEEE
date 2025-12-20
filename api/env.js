c42d114f362a54443b8ca76f520610702d8c32c5
machineblade <machine.blade2991@gmail.com>
2025-12-20 16:04:31 -0500

    Add files via upload

--- api/env.js
+++ api/env.js
@@ -1,9 +1,8 @@
 // api/env.js
-export default async function handler(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json({
-      SUPABASE_URL: process.env.SUPABASE_URL,
-      SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY
-    });
-  }
-  
+export default function handler(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).json({
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY
+  });
+}
